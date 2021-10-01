07f5b5d7db5b776feecd5b101f14252f0d867607
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 17:12:44 +1000

    f

--- commands/kills.js
+++ commands/kills.js
@@ -1,4 +1,4 @@
-const footer = 'Rusty made by sun#0420, Information gathered from rustlabs.com, Steam API';
+const footer = 'Rusty made by sun#0420, Information gathered from Steam API';
 const title = 'Shows stats of the given STEAMID64 for Rust'
 const icon1 = 'https://i.redd.it/gzrqicukid0z.jpg';
 const color = 0x272a21;
@@ -11,6 +11,8 @@ module.exports = {
     execute(message, args) {
         if(!args.length) return;
         if(args[0] == null) return;
+        if(isNaN(args[0])) return;
+        if(args[0].length != 17) return;
         console.log('idgiven: ' + args);
         let url = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=252490&key=4A3DA3C2CEB5617EF8660D9A09FC39EB&steamid=' + args;
         function getPosts () {
